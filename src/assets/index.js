73564941042d9ceaa82ed888bd148283872fe5b0
Tomek <tomek.bielicki@gmail.com>
2023-10-03 18:07:29 +0200

    Assets

---
+++ src/assets/index.js
@@ -0,0 +1,12 @@
+import mfAvatar from "./mfAvatar.svg";
+import heroDevices from "./heroDevices.svg";
+import wlabadiMainPage from "./wlabadiMainPage.png";
+import krainaPieknaMainPage from "./krainaPieknaMainPage.png";
+import batixMainPage from "./batixMainPage.png";
+export {
+  mfAvatar,
+  heroDevices,
+  wlabadiMainPage,
+  krainaPieknaMainPage,
+  batixMainPage,
+};
