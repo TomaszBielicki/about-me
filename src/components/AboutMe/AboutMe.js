5ba4f5a0d2d33dca3b036c740c0adb9f7667b7c1
Tomek <tomek.bielicki@gmail.com>
2023-10-03 18:08:00 +0200

    About me section

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Wrapper, Title, Text } from "./AboutMe.style";
+
+function AboutMe() {
+  return (
+    <Wrapper>
+      <Title>Hi, I’m Tom. Nice to meet you. </Title>
+      <Text>
+        Since beginning my journey as a freelance designer over 11 years ago,
+        I've done remote work for agencies, consulted for startups, and
+        collaborated with talented people to create digital products for both
+        business and consumer use. I'm quietly confident, naturally curious, and
+        perpetually working on improving my chops one design problem at a time.
+      </Text>
+    </Wrapper>
+  );
+}
+
+export default AboutMe;
---
+++ src/components/AboutMe/AboutMe.style.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 10% 20% 10% 20%;
+`;
+
+export const Title = styled.h1`
+  font-size: 2rem;
+`;
+
+export const Text = styled.p`
+  font-size: 1.25rem;
+  line-height: 1.5;
+  text-align: center;
+  word-break: break-all;
+`;
