202f3d7f52eb1bbfbcd892f08076de5f41d0114c
Tomek <tomek.bielicki@gmail.com>
2023-10-03 18:08:35 +0200

    Portfolio section and styles

---
+++ src/components/Portfolio/Potrfolio.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Bar from "../PortfolioBar/Bar";
+import { Wrapper } from "./Portfolio.style";
+function Potrfolio() {
+  return (
+    <Wrapper>
+      <Bar />
+    </Wrapper>
+  );
+}
+
+export default Potrfolio;
