49fd1c7f708f1de8a4f9fbc2b897609dd93eaea5
Tomek <tomek.bielicki@gmail.com>
2023-10-24 20:32:17 +0200

    Header prepare

---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "./Header.style";
+
+function Header() {
+  return <Text>Menu</Text>;
+}
+
+export default Header;
---
+++ src/components/Header/Header.style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Text = styled.p`
+  background-color: white;
+  color: #6e07f3;
+  text-align: right;
+`;
